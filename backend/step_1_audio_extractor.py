f8612c06fba313813df14a43efe2c14c807f3834
akhilaboddula <akhilaboddula16@gmail.com>
2026-05-23 00:07:46 +0530

    Fix ffmpeg path for Streamlit Cloud

--- backend/step_1_audio_extractor.py
+++ backend/step_1_audio_extractor.py
@@ -1,15 +1,13 @@
 import os
 import subprocess
 
-FFMPEG_PATH = r"C:\Users\karth\AppData\Local\Microsoft\WinGet\Packages\Gyan.FFmpeg_Microsoft.Winget.Source_8wekyb3d8bbwe\ffmpeg-8.1.1-full_build\bin\ffmpeg.exe"
-
 
 def extract_audio_from_video(video_path: str, output_audio_path: str) -> str:
     video_path = os.path.abspath(video_path)
     output_audio_path = os.path.abspath(output_audio_path)
 
     command = [
-        FFMPEG_PATH,
+        "ffmpeg",
         "-y",
         "-i", video_path,
         "-vn",
